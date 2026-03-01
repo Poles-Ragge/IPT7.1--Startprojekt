4d0343a518110d1cda9802f0b1a5e2900f82424c
Luka Vucer <vucerluka@gmail.com>
2026-03-01 22:48:43 +0100

    added POST request to login

--- src/Website/javascript.js
+++ src/Website/javascript.js
@@ -1,13 +1,12 @@
-
-document.querySelector("form").addEventListener("submit", async function(e) {
+document.querySelector("form")?.addEventListener("submit", async function(e) { ///Needed to change this. Wouldn't work otherwise because i have two diff js codes in one file. I lost 30 mins on this bullshit
     e.preventDefault();
 
     const data = {
-        username: document.getElementById("username").value,
-        password: document.getElementById("password").value,
-        firstname: document.getElementById("firstname").value,
-        lastname: document.getElementById("lastname").value,
-        language: document.querySelector('input[name="lang"]:checked').value.toLowerCase()
+        username: document.getElementById("username")?.value,
+        password: document.getElementById("password")?.value,
+        firstname: document.getElementById("firstname")?.value,
+        lastname: document.getElementById("lastname")?.value,
+        language: document.querySelector('input[name="lang"]:checked')?.value?.toLowerCase()
     };
 
     try {
@@ -26,5 +25,36 @@ document.querySelector("form").addEventListener("submit", async function(e) {
         console.error("Error:", error);
         alert("fail");
     }
+
+});
+
+document.getElementById("anmelde")?.addEventListener("submit", async function (e) { ///Why does this shit work but NOT my idea jesus christ help me b4 i punch dat screen
+    e.preventDefault();
+
+    const data = {
+        username: document.getElementById("username")?.value,
+        password: document.getElementById("passwort")?.value 
+    };
+
+    try {
+        const response = await fetch("https://ipt71.kuno-schuerch.bbzwinf.ch/user/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }); 
+
+        if (!response.ok) {
+            throw new Error("fail");
+        }
+        const result = await response.json();
+        console.log(result);
+        alert("gut");
+    } catch (error) {
+        console.error("Eror:", error);
+        alert("fail");
+    }
+
 
 });
